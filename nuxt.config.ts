84672a98bb0f874af8d7e585db93f3ed376c3076
adiaskaputra <gadikpepe76@gmail.com>
2025-01-22 15:17:50 +0700

    feat: nuxt-security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,11 +12,9 @@ export default defineNuxtConfig({
       ],
     },
   },
-
   devtools: {
     enabled: false,
   },
-
   modules: [
     [
       '@pinia/nuxt',
@@ -24,18 +22,16 @@ export default defineNuxtConfig({
         autoImports: ['defineStore', 'storeToRefs'],
       },
     ],
+    'nuxt-security',
   ],
-
   imports: {
     dirs: ['composables/**'],
   },
-
   css: [
     '~/assets/css/tailwind.css',
     '~/assets/css/transition.css',
     '~/assets/css/main.css',
   ],
-
   postcss: {
     plugins: {
       'postcss-hexrgba': {},
@@ -45,7 +41,6 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-
   typescript: {
     tsConfig: {
       compilerOptions: {
@@ -54,10 +49,38 @@ export default defineNuxtConfig({
       },
     },
   },
-
+  security: {
+    nonce: true,
+    rateLimiter: false,
+    csrf: true,
+    headers: {
+      contentSecurityPolicy: {
+        'script-src': [
+          "'self'", // Fallback value, will be ignored by most modern browsers (level 3)
+          "'unsafe-inline'", // Fallback value, will be ignored by almost any browser (level 2)
+          "'strict-dynamic'", // Strict CSP via 'strict-dynamic', supported by most modern browsers (level 3)
+          "'nonce-{{nonce}}'", // Enables CSP nonce support for scripts in SSR mode, supported by almost any browser (level 2)
+        ],
+        'style-src': [
+          "'self'", // Enables loading of stylesheets hosted on same origin
+          'fonts.googleapis.com',
+          "'unsafe-inline'", // Recommended default for most Nuxt apps
+        ],
+        'base-uri': ["'none'"],
+        'img-src': ["'self'", 'data:'], // Add relevant https://... sources if you load images from external sources
+        'font-src': ["'self'", 'fonts.gstatic.com'],
+        'object-src': ["'none'"],
+        'script-src-attr': ["'none'"],
+        'frame-ancestors': ["'self'"],
+        'upgrade-insecure-requests': true,
+      },
+      permissionsPolicy: {
+        camera: ['self'],
+      },
+    },
+  },
   devServer: {
     port: 8000,
   },
-
   compatibilityDate: '2025-01-09',
 })
