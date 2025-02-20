01d917fb8f1f09eca5acb5e4d4282198b7ab6158
adiaskaputra <gadikpepe76@gmail.com>
2025-02-20 12:24:51 +0700

    chore: add docker-file, node-version

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>B O I L E R P L A T E</div>
+  <div class="bg-gray-200 text-gray-700 font-bold px-3 py-2">
+    Nuxt 3 + Tailwind 4
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
